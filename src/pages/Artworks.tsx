b0b1ba0fc5ce4c46801ad6c11d2cee17086dc348
alexwes <pampawes@gmail.com>
2025-07-12 16:55:52 -0400

    Fix video overlay to allow video controls

--- src/pages/Artworks.tsx
+++ src/pages/Artworks.tsx
@@ -55,11 +55,13 @@ const Artworks = () => {
                     />
                   )}
                   {/* Overlay */}
-                  <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-50 transition-all duration-300 flex items-center justify-center">
-                    <div className="text-white text-2xl font-light tracking-wider opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                      {artwork.artist}
+                  {!artwork.video && (
+                    <div className="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-50 transition-all duration-300 flex items-center justify-center">
+                      <div className="text-white text-2xl font-light tracking-wider opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                        {artwork.artist}
+                      </div>
                     </div>
-                  </div>
+                  )}
                 </div>
               </div>
             ))}
